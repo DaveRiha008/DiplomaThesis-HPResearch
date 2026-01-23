8fc5374c04d3227db2933b54936e955aa4e293bb
David Říha <davedave0008@gmail.com>
2026-01-23 13:24:52 +0100

    Added enemy orc animation and movement

--- HPResearchGame/Assets/Scripts/EnemyController.cs
+++ HPResearchGame/Assets/Scripts/EnemyController.cs
@@ -1,9 +1,14 @@
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public int maxHP = 3;
+	const string animXMoveID = "MoveX";
+	const string animYMoveID = "MoveY";
+
+    [Header("Combat information")]
+	public int maxHP = 3;
     public int currentHP;
 
     [Header("Tween information")]
@@ -12,30 +17,65 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     Color afterHitColor = Color.red;
 
-
-
-
     Color originalColor;
 
+    //AI Navigation variables
+    [Header("AI Navigation")]
+	[SerializeField]
+    Transform target;
+    [SerializeField]
+    float minDistanceToTarget = 2f;
+
+	//Components
+	NavMeshAgent agent;
 	SpriteRenderer sr;
+    Animator animator;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
     {
         currentHP = maxHP;
 
-        sr = GetComponent<SpriteRenderer>();
+        agent = GetComponent<NavMeshAgent>();
+		sr = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 
-        originalColor = sr.color;
+		//Agent setup
+		agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.stoppingDistance = minDistanceToTarget;
+
+
+		originalColor = sr.color;
 	}
 
     // Update is called once per frame
     void Update()
     {
-        
+        RetargetDestination();
+
+        SetAnimationMove();
+	}
+
+    void SetAnimationMove()
+    {
+		Vector3 velocity = agent.velocity.normalized;
+		float ratio = Mathf.Abs(velocity.x) + Mathf.Abs(velocity.y); //Get the ratio to normalize diagonal movement
+		if (ratio == 0) ratio = 1f; //Avoid division by zero
+
+        //Normalize, so the sum always equals 1
+		animator.SetFloat(animXMoveID, velocity.x / ratio); 
+		animator.SetFloat(animYMoveID, velocity.y / ratio);
+	}
+
+    void RetargetDestination()
+    {
+
+        agent.SetDestination(target.position);
     }
 
-    public void GetHit(int damage)
+
+	public void GetHit(int damage)
     {
         currentHP -= damage;
 
